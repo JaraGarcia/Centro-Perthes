31c7dee974fc6e0617666ef3935a5162767ae034
Jara García <jara.bilbao14@gmail.com>
2022-11-07 08:29:56 +0000

    subscribe button

--- src/components/EscuelaFamilias/EFform.vue
+++ src/components/EscuelaFamilias/EFform.vue
@@ -1,14 +1,15 @@
 <template>
-<div class="subscribe-container">
+<div>
+  <p>Si quieres estar al día de la información sobre nuestra clínica suscríbete a nuestra newsletter</p>
   <input
             type="email"
             v-model="email"
             class="subscribe-container__email"
-            placeholder="Your email address"
+            placeholder="Tu correo electrónico"
             required
           />
           <div>
-            <button
+            <button class="button" 
               @click="submit"
               :class="['subscribe-container__submit', !email ? 'disabled' : '']"
             >
@@ -20,35 +21,11 @@
 
 
 <script>
-import axios from 'axios'
-import mailchimp from '@mailchimp/mailchimp_marketing'
 
-mailchimp.setConfig({
-  apiKey: '724f03f0dc2ea16ad4782e7ce932379d-us14',
-  server: 'us14'
-})
+import { http } from '@/plugins/axios'
 
 export default {
-
     name: 'EFform',
-    props: {
-        elementId: {
-            type: String,
-            required: true
-        },
-        url: {
-            type: String,
-            required: true
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        subtitle: {
-            type: String,
-            required: false
-        }
-        },
     data () {
         return {
           email: ''
@@ -56,73 +33,46 @@ export default {
     },
     methods: {
       submit: async function () {
-        const {data} = await axios.post('https://us14.api.mailchimp.com/3.0/lists/6d07b49f0b/members', {
+        const {data} = await http.post('/add-member', {
           email_address: this.email,
           status: 'subscribed'
-        }, {
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            authorization: `Bearer 724f03f0dc2ea16ad4782e7ce932379d-us14`
-          }
         })
         console.log(data)
       }
     }
 }
 
-// export const Base = () => ({
-//   props: {
-//     elementId: {
-//       type: String,
-//       default: text("elementId", "first-email-signup-form")
-//     },
-//     url: {
-//       type: String,
-//       default: text("url", "https://us14.list-manage.com/subscribe/post?u=XXXXXXXXXXXXXX")
-//     },
-//     title: {
-//       type: String,
-//       default: text("title", "Subscribe to the Newsletter")
-//     },
-//     subtitle: {
-//       type: String,
-//       default: text("subtitle", "We take privacy seriously and we will never spam or sell your information.")
-//     },
-//   }
-
 </script>
 <style scoped>
 
-.subscribe-container {
-  border: black;
-  border-radius: 4px;
-  box-shadow: 0 2px 15px 0 rgba(210, 214, 220, 0.3);
-  display: flex;
-  flex-direction: column;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",
-    "Helvetica Neue", sans-serif;
-  font-size: 16px;
-  line-height: 1.5;
-  margin-bottom: 2rem;
-  max-width: 600px;
-  padding: 1.25rem 1rem;
-  width: 100%;
+
+input{
+  margin: 5px;
+  width: 200px;
 }
-form {
-    margin-bottom: 0;
-  }
-form title{
-    color: black;
-    display: inline-block;
-    font-size: 1.75rem;
-    line-height: 1.2;
-    margin-bottom: 0.5rem;
-  
+
+button {
+  border: transparent;
 }
 
 .disabled {
   opacity: .5;
   pointer-events: none;
+}
+
+
+.button {
+  display: inline-block;
+  border-radius: 4px;
+  background-color: #A3B5C0;;
+  border: none;
+  color: #FFFFFF;
+  text-align: center;
+  padding: 10px;
+  width: 200px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
 }
 
 </style>
--- src/views/EscuelaFamilias.vue
+++ src/views/EscuelaFamilias.vue
@@ -1,42 +1,18 @@
 <template>
-    <div class="container">
-        <form>
-          <label for="carnet">¿Tienes alguna duda? Contacta con nosotros</label>
-            <input type="text" v-model.lazy='carnet'>
-            <label for='nombre'>Nombre:</label>
-            <input type="text" v-model.lazy='nombre'>
-
-            <label for="apellidos">Apellido:</label>
-            <input type="text" v-model.lazy='apellidos'>
-           
-            <label for="edad">Edad:</label>
-            <input type="text" v-model.lazy='edad'>
-        </form>
-        <hr>
-
-    <EFform/>
-    </div>
+<EFsemaforo/>
+<EFform/>
 </template>
 
 
 <script>
-
+import EFsemaforo from '@/components/EscuelaFamilias/EFsemaforo.vue';
 import EFform from '../components/EscuelaFamilias/EFform.vue';
 export default {
   name: 'EscuelaFamilias',
   components: {
+    EFsemaforo,
     EFform,
   },
 }
 
 </script>
-
-<style>
-form{
-    display: flex;
-    flex-direction: column;
-    margin: 10px 5px 30px;
-    gap: 10px;
-    text-align: left;
-}
-</style>
