68af56755e941b7cb78acbe2e9dc30f2570b57d7
Jara García <jara.bilbao14@gmail.com>
2022-11-14 08:11:03 +0000

    cambios Susana

--- src/components/FooterPlace.vue
+++ src/components/FooterPlace.vue
@@ -1,55 +1,67 @@
 <template>
-
-<div id="container">
-
-
-
-<h8> <h4>También puedes contactar con nosotros en:</h4>
-    Nuestro nº de telefono 633.33.33.33: <a href="tel:633333333">☎ Teléfono </a><br>
-    <p> Enviarnos un correo electrónico: 
-    <a  href="mailto: indev.cip.perthes@gmail.com">click aqui✉</a> </p>
-    <h7><router-link to="/politica_privacidad" >Política de Privacidad </router-link></h7>
-    <p>© year 2022 ,all rights reserved</p>
-</h8>
-</div>
+    <div class="containerfooter">
+        <div class="logofooter">
+         <img src="../assets/logo.png"  alt="logoclinica" width="50" height="50" />
+         <h6 >30 años de experiencia.</h6> <br>
+         <h6 >Miembros de :</h6>
+         <h6><a href="https://secipe.org/" target="_blank" >SECIPE</a></h6>
+         </div>
+         <div class="principal">
+         <h8> <h4>También puedes contactar con nosotros en:</h4>
+         Nuestro nº de teléfono 633.33.33.33: <a href="tel:63333333">:phone: Teléfono </a><br>
+         <p> Enviarnos un correo electrónico:
+         <a  href="mailto: indev.cip.perthes@gmail.com">click aqui:email:</a> </p>
+         <h7><router-link to="/politica_privacidad" >Política de Privacidad </router-link></h7>
+         <p>© year 2022 ,all rights reserved</p>
+         </h8>
+         </div>
+    </div>
 </template>
-
-
 <script>
 export default {
     name: 'FooterPlace',
-    
 }
 </script>
-
-
 <style scoped>
-
-
-
- h8{
-        color: rgb(27, 21, 21);
-        text-align:center;
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
-        border-color: rgb(35, 35, 187);
-        padding: 10px;
-    position:absolute;
+.containerfooter{
+    border:3px solid rgb(184, 161, 161);
+    width: 1000px;
+    width: 99.9%;
+    background-size:auto;
+    margin: auto ;
+    display: flex;
+    background:#A3B5C0 ;
+}
+.logofooter{
+    width: 20%;
+    height:auto;
+    min-height: 227px;
+    text-align:center;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    padding: 10px;
+    margin:  auto;
+    border: 15px solid rgba(50,50,50,0.3);
+    clear:both;
+    padding-top: 10px;
+    overflow: hidden;
+    font-size: 12px;
+    border-radius: 10%;
+    background:#A3B5C0 ;
+    }
+.principal{
+    width: 80%;
+    height: auto;
+    text-align:center;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    padding: 10px;
     margin:  auto;
     background: #A3B5C0;
-    background-image: linear-gradient(top, #191919,#000000);
-    border: 10px solid rgba(50,50,50,0.3);
-    width:100%;
+    border: 15px solid rgba(50,50,50,0.3);
     clear:both;
-    margin-top: 0px;
-    padding-top: 1px;
-    overflow: visible;
+    padding-top: 10px;
+    overflow: hidden;
     font-size: 18px;
-
-
+    border-radius: 1%;
+    background-color: rgb(151, 166, 190);
     }
-
-
-
-
 </style>
-
